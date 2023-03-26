5547c90eeadd3593068f372c3b8247d27009055d
sinthushan <sinthushan@gmail.com>
2023-03-26 11:17:39 -0400

    completed models

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+  foreignKey: 'author_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'author_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Comment };
